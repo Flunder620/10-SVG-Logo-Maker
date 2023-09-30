8721e6f4ad00cdb61ac18b21edcf4a746276261e
Flunder620 <jguzman12310@yahoo.com>
2023-09-30 14:45:49 -0500

    added array of questions for inquirer

---
+++ index.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter your text: "
+    },
+    {
+        type: "input",
+        name: "tcolor",
+        message: "What color what you like your text?(Red, Green, Blue)"
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "Which shape would you like?(Circle, Triangle, or Square)"
+    },
+    {
+        type: "input",
+        name: "scolor",
+        message: "What color would you like your shape?(Red, Green, Blue)"
+    }
+];
+
+inquirer.createPromptModule(questions)
